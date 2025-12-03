ab89a35caec5b53b71ed2d83d4efd822942cd07e
João Araujo <joaomigueldacostaaraujo@gmail.com>
2025-12-03 13:50:00 -0300

    rotas stock + integracao

--- mobile/src/api/stock.js
+++ mobile/src/api/stock.js
@@ -5,29 +5,19 @@ export async function confirmVoiceItems(selections) {
   return data;
 }
 
-// const HARD_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0aGFpcy5wYWl2YUBnbWFpbC5jb20iLCJleHAiOjE3NjQxODEwODR9.OXKLKxQDB4gf3w8u5Flv008ziMRU9evZujp54L2Y9ZY";
-
-// export async function confirmVoiceItems(selections) {
-//   const { data } = await api.post(
-//     "/stock/confirm-voice",
-//     selections,
-//     {
-//       headers: {
-//         Authorization: `Bearer ${HARD_TOKEN}`,
-//       }
-//     }
-//   );
-//   return data;
-// }
-
 export async function fetchStock({ q } = {}) {
   const params = {};
   if (q) params.q = q;
 
-  const { data } = await api.get("/stock/list", {
-    params
-  });
-
+  const { data } = await api.get("/stock/list", { params });
   return data;
 }
 
+export async function updateStockItem(id, payload) {
+  const { data } = await api.patch(`/stock/item/${id}`, payload);
+  return data;
+}
+
+export async function deleteStockItem(id) {
+  await api.delete(`/stock/item/${id}`);
+}
